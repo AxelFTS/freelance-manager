2f8354b46bb748cfb87556914aff9d30a3b4ecdf
Axel <fuentesaxel07@gmail.com>
2025-12-18 18:12:45 +0100

    [Front] feat(#15): proxy front configuration

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -7,6 +7,7 @@ import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -14,5 +15,6 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAnimations(),
+    provideHttpClient(),
   ],
 };
