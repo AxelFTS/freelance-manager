8f1424db64ca9e61d63a41126ec6eec1b82fe607
Axel <fuentesaxel07@gmail.com>
2025-12-23 18:22:28 +0100

    [Front] feat(#18): Create page list of all Clients

--- frontend/src/app/features/clients/pages/client-list-page/client-list-page.ts
+++ frontend/src/app/features/clients/pages/client-list-page/client-list-page.ts
@@ -1,11 +1,88 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  inject,
+  ViewChild,
+  AfterViewInit,
+  signal,
+} from '@angular/core';
+import { DatePipe } from '@angular/common';
+import { Router } from '@angular/router';
+import { MatTableModule, MatTableDataSource } from '@angular/material/table';
+import { MatPaginatorModule, MatPaginator } from '@angular/material/paginator';
+import { MatSortModule, MatSort } from '@angular/material/sort';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { Client } from '../../models/client.model';
+import { ClientService } from '../../../../core/services/client.services';
 
 @Component({
   selector: 'app-client-list-page',
-  imports: [],
+  imports: [
+    DatePipe,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
   templateUrl: './client-list-page.html',
   styleUrl: './client-list-page.scss',
 })
-export class ClientListPage {
+export class ClientListPage implements AfterViewInit {
+  private clientService = inject(ClientService);
+  private router = inject(Router);
 
+  clients = signal<Client[]>([]);
+
+  dataSource = new MatTableDataSource<Client>([]);
+
+  displayedColumns: string[] = [
+    'nom',
+    'email',
+    'telephone',
+    'createdAt',
+    'actions',
+  ];
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  ngOnInit() {
+    this.loadClients();
+  }
+
+  ngAfterViewInit(): void {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  loadClients() {
+    this.clientService.getAllClients().subscribe({
+      next: (clients) => {
+        this.clients.set(clients);
+        this.dataSource.data = clients;
+      },
+      error: (err) => {
+        console.error('Erreur', err);
+      },
+    });
+  }
+
+  newClient() {
+    this.router.navigate(['/clients/new']);
+  }
+
+  editClient(id: number) {
+    this.router.navigate(['/clients/' + id + '/edit']);
+  }
+
+  deleteClient(id: number) {
+    if (confirm('Êtes-vous sûr ?')) {
+      this.clientService.deleteClient(id).subscribe({
+        next: () => {
+          this.loadClients();
+        },
+      });
+    }
+  }
 }
