ef6a3fa0a58104a3bdf05eee2e43bbc22a73ce6c
Axel <fuentesaxel07@gmail.com>
2026-02-02 15:50:55 +0100

    [Front] feat(#32): Créer le service FactureService

---
+++ frontend/src/app/core/services/facture.service.ts
@@ -0,0 +1,49 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable, signal } from '@angular/core';
+import { Observable, tap } from 'rxjs';
+import {
+  CreateFacture,
+  Facture,
+  StatutFacture,
+} from '../../features/factures/models/facture.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class FactureService {
+  private http = inject(HttpClient);
+  private factures = signal<Facture[]>([]);
+  private facture = signal<Facture | null>(null);
+  private url = '/api/';
+
+  getAllFactures(): Observable<Facture[]> {
+    return this.http
+      .get<Facture[]>(this.url + 'factures')
+      .pipe(tap((facture) => this.factures.set(facture)));
+  }
+
+  getById(id: number): Observable<Facture> {
+    return this.http
+      .get<Facture>(this.url + 'factures/' + id)
+      .pipe(tap((facture) => this.facture.set(facture)));
+  }
+
+  createFacture(facture: CreateFacture): Observable<Facture> {
+    return this.http.post<Facture>(this.url + 'factures', facture);
+  }
+
+  updateFacture(id: number, facture: CreateFacture): Observable<Facture> {
+    return this.http.put<Facture>(this.url + 'factures/' + id, facture);
+  }
+
+  deleteFacture(id: number): Observable<void> {
+    return this.http.delete<void>(this.url + 'factures/' + id);
+  }
+
+  updateStatut(id: number, statut: StatutFacture): Observable<Facture> {
+    return this.http.patch<Facture>(
+      this.url + 'factures/' + id + '/statut',
+      statut
+    );
+  }
+}
